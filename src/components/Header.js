9ed1dabcfdacec9015c4e6abf836e66d8d5dbb1b
Ali Emir Şen <senaliemir@gmail.com>
2019-05-21 17:03:07 +0300

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="app-header">
+      <h1 className="app-header-title">React Recipe App</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Header from "./components/Header";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
